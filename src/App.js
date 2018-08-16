a4977080d31112a8a4042bc3b1beb0968a1fad05
Lee Graham <butchqueenindrags@Lees-MacBook-Pro.local>
2018-08-16 12:19:01 -0600

    Hopefully staging this from scratch successfully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchForm from './SearchForm.js';
+import CardContainer from './CardContainer.js';
+import ComparisonContainer from './ComparisonContainer.js';
+import DistrictRepository  from './helper.js';
+import kinderData from './data/kindergartners_in_full_day_program.js';
+const districts = new DistrictRepository(kinderData);
 
 class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			districts: []
+		}
+	}
+
   render() {
-    return (
-      <div>Welcome To Headcount 2.0</div>
-    );
-  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome To Headcount 2.0</h1>
+        <SearchForm />
+      </header>
+      <ComparisonContainer />
+      <CardContainer />
+    </div>
+  );
+}
 }
 
 export default App;
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,34 @@
-export default class DistrictRepository {
+import kinderData from './data/kindergartners_in_full_day_program.js';
 
+
+export default class DistrictRepository {
+  constructor(){
+  }
+  
+  stats = kinderData.reduce((accu, district) => {
+    const locale = district.Location
+    if(isNaN(district.Data)){
+      district.Data = 0
+    }
+    const schoolInfo = {[district.TimeFrame]: Number(parseFloat(district.Data).toFixed(3))}
+    if(!accu[locale]){
+      accu[locale] = schoolInfo
+    }
+    accu[locale]= {...schoolInfo, ...accu[locale]}
+    return accu
+  }, {})
+
+  findByName = (district='') => {
+    const newDistrict = district.toUpperCase()
+    const objKeys = Object.keys(this.stats)
+    const upperObjKeys = objKeys.map(key => key.toUpperCase())
+    if (!district || objKeys.includes(!district)) {
+      return undefined
+    }
+    if(newDistrict && upperObjKeys.includes(newDistrict)){
+      const newObj = Object.assign({}, {[newDistrict]: this.stats[newDistrict]})
+      console.log(newObj)
+      return newObj
+    }
+  }
 }

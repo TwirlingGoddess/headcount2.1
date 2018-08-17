299a74f469be0ee53e2a1e22618bf65632b6f6ba
Lee Graham <graham.la3@gmail.com>
2018-08-17 00:01:04 -0600

    Setup initial card container test structure

---
+++ src/CardContainer.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CardContainer from './CardContainer';
+
+describe('CardContainer', () => {
+
+	it('should....', () => {
+		expect().toBe()
+	})
+	
+})

5fb92b9fae88b8ee5869a0e312b3024cbe9ee043
Lee Graham <graham.la3@gmail.com>
2018-08-17 00:17:32 -0600

    Setup initial search field structure

---
+++ src/SearchForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			value: ''
+		}
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			value: event.target.value
+		})
+	}
+
+	render() {
+		return(
+			<form>
+				<input 	type='text' 
+						value={this.state.value} 
+						placeholder="Type in district to Search"
+						onChange={this.handleChange}
+				/>
+			</form>
+		)
+	}
+}

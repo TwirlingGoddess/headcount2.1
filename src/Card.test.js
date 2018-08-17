94e4b28585c1418dd003a6e8f464baae21cef269
Lee Graham <graham.la3@gmail.com>
2018-08-16 23:51:54 -0600

    Setup initial card test structure

---
+++ src/Card.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+describe('Card', () => {
+
+	it('should match the snapshot', () => {
+		const wrapper = shallow(<Card />)
+		expect(wrapper).toMatchSnapshot()
+	})
+
+	// it('should', () => {
+	// 	expect(wrapper).toBe()
+	// })
+
+})

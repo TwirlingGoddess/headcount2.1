19ee1458684c0cab0dadf564534ac89d4dc71e24
Lee Graham <graham.la3@gmail.com>
2018-08-19 11:16:03 -0600

    Edited initial sanitation code to filter the data

--- src/helper.js
+++ src/helper.js
@@ -1,38 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import kinderData from './data/kindergartners_in_full_day_program.js';
 import PropTypes from 'prop-types'
-
 
 class DistrictRepository extends Component {
   constructor(){
     super()
+
+    this.stats = this.populateCards(kinderData)
   }
   
-  stats = kinderData.reduce((accu, district) => {
-    const locale = district.Location
-    if(isNaN(district.Data)){
-      district.Data = 0
-    }
-    const schoolInfo = {[district.TimeFrame]: Number(parseFloat(district.Data).toFixed(3))}
-    if(!accu[locale]){
-      accu[locale] = schoolInfo
-    }
-    accu[locale]= {...schoolInfo, ...accu[locale]}
-    return accu
-  }, {})
+  populateCards = (kinderData) => {
+    return kinderData.reduce((accu, district) => {
+      const locale = district.Location.toUpperCase()
+      const schoolYear = district.TimeFrame
+      const roundedNum = Number(parseFloat(district.Data).toFixed(3))
+      if(!accu[locale]){
+        accu[locale] = {locale, stats:{}}
+      }
+      accu[locale].stats[schoolYear] = roundedNum || 0
+      return accu
+    }, {})
+  }
 
-  findByName = (districtName='') => {
-    const newDistrict = district.toUpperCase()
-    const objKeys = Object.keys(this.stats)
-    const upperObjKeys = objKeys.map(key => key.toUpperCase())
-    if (!district || objKeys.includes(!district)) {
-      return undefined
-    }
-    if(newDistrict && upperObjKeys.includes(newDistrict)){
-      const newObj = Object.assign({}, {[newDistrict]: this.stats[newDistrict]})
-      return newObj
+  findByName = (districtName = '') => {
+    const upperDistrict = districtName.toUpperCase();
+    
+    if(!this.stats[upperDistrict]) {
+      return undefined} 
+
+    if (this.stats[upperDistrict]) {
+      return this.stats[upperDistrict]
     }
   }
+
+
+
+
+  // findByName = (districtName='') => {
+  //   const objKeys = Object.keys(this.stats)
+  //   objKeys.reduce((districtListObj, key) => {
+  //     if (!districtName) {
+  //       return undefined
+  //     }
+  //     if (!key.toUpperCase()!==districtName.toUpperCase()) {
+  //       return undefined
+  //     }
+  //     if(key.toUpperCase()===districtName.toUpperCase()) {
+  //       districtListObj = {[key]: this.stats[key]};
+  //         // console.log(updatedObj)
+  //       // return updatedObj
+  //     }
+  //     Object.assign(updatedObj, districtListObj)
+  //     return districtListObj
+  //   }, {})
+  //   return updatedObj
+  // }
 
   findAverage = (district) => {
     console.log(district)
